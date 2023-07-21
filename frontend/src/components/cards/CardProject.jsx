d0904e6d4871800c4f238f2a4d7ba1f95a822b5b
Jayson_Delion <jayson-delion_student2023@wilder.school>
2023-07-21 23:17:37 +0200

    wcs projects added

--- frontend/src/components/cards/CardProject.jsx
+++ frontend/src/components/cards/CardProject.jsx
@@ -2,15 +2,19 @@ import snakeGame from "../../assets/snakeGame.png";
 import omnifood from "../../assets/omnifood.png";
 import travelAgency from "../../assets/travelAgency.png";
 import doruido from "../../assets/doruido.png";
+import flipperHome from "../../assets/flipperHome.png";
+import hackathonAlibi from "../../assets/hackathonAlibi.png";
 
 export default function CardProject() {
-  const projects = ["Omnifood", "Travel Agency", "Snake", "Doruido"];
-  const projectImages = [omnifood, travelAgency, snakeGame, doruido];
+  const projects = ["Omnifood", "Travel Agency", "Snake", "Doruido", "Hackathon Alibi", "Flipper"];
+  const projectImages = [omnifood, travelAgency, snakeGame, doruido, hackathonAlibi, flipperHome];
   const projectsTags = [
     ["html", "css", "javascript"],
     ["html", "css", "javascript"],
     ["html", "css", "javascript"],
-    ["react", "sass", "node", "mysql"]
+    ["react", "sass", "node", "mysql"],
+    ["react", "sass"],
+    ["html", "css", "javascript"],
   ];
 
   const projectsURL = [
@@ -20,10 +24,12 @@ export default function CardProject() {
   ];
 
   const projectsDescription = [
-    "Welcome to the Omnifood project repository! This is a landing page for our premium food delivery service, where we prioritize quality, convenience, and healthy eating.",
-    "Welcome to the Travel Agency project repository! This is a landing page for a travel booking planner agency.",
-    "Welcome to the Snake Game project! This is a JavaScript implementation of the classic Snake game, where you control a snake to eat food and grow longer without hitting the walls or yourself.",
-    "Welcome to Doruido! This is a personal project based on a passion of mine, Japanimation. It will provide a platform to get cultural news about animes, mangas and Japanese art scene."
+    "Welcome to Omnifood ! This is a landing page for our premium food delivery service, where we prioritize quality, convenience, and healthy eating.",
+    "Welcome to Travel Agency ! This is a landing page for a travel booking planner agency.",
+    "Welcome to the Snake Game ! This is a JavaScript implementation of the classic Snake game, where you control a snake to eat food and grow longer without hitting the walls or yourself.",
+    "Welcome to Doruido ! This is a personal project based on a passion of mine, Japanimation. It will provide a platform to get cultural news about animes, mangas and Japanese art scene.",
+    "Welcome to the Alibi Hackathon! First participation in the Wild Code School Hackathon, this mini project was carried out in teams of 5 in 24 hours. Landing page for an atypical travel agency... Who knows, you might just find what you're looking for!",
+    "Welcome to Flipper ! Project based on the visual and interactive reproduction of a social network (Twitter). Group work at Wild Code School."
   ];
 
   const badgeType = (tag) => {
@@ -57,7 +63,7 @@ export default function CardProject() {
             <img className="object-contain" src={projectImages[i]} alt={project} />
           </figure>
           <div className="card-body">
-            <h2 className="card-title mb-5">{project}</h2>
+            <h2 className="card-title mb-5 text-2xl">{project}</h2>
             <p className="leading-7 w-fit">{projectsDescription[i]}</p>
             <div className="card-actions mt-5">
               {projectsTags[i].map((tag, i) => (
@@ -74,9 +80,7 @@ export default function CardProject() {
                 Visit
               </a>
             ) : (
-              <a data-theme="cmyk" href="" className="btn btn-disabled font-bold text-xl">
-                Under Construction
-              </a>
+              <div className="bg-base-100 px-5 py-3 font-bold text-xl text-center h-100">Coming Soon</div>
             )}
           </div>
         </div>
