d579d4052669d43bb77beed0826b459b7287de53
Jayson_Delion <jayson-delion_student2023@wilder.school>
2023-07-25 11:58:37 +0200

    card projects created from DB data

--- frontend/src/components/cards/CardProject.jsx
+++ frontend/src/components/cards/CardProject.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import snakeGame from "../../assets/snakeGame.png";
 import omnifood from "../../assets/omnifood.png";
 import travelAgency from "../../assets/travelAgency.png";
@@ -6,50 +8,33 @@ import flipperHome from "../../assets/flipperHome.png";
 import hackathonAlibi from "../../assets/hackathonAlibi.png";
 
 export default function CardProject() {
-  const projects = ["Omnifood", "Travel Agency", "Snake", "Doruido", "Hackathon Alibi", "Flipper"];
   const projectImages = [omnifood, travelAgency, snakeGame, doruido, hackathonAlibi, flipperHome];
-  const projectsTags = [
-    ["html", "css", "javascript"],
-    ["html", "css", "javascript"],
-    ["html", "css", "javascript"], 
-    ["react", "sass", "node", "mysql"],
-    ["react", "sass"],
-    ["html", "css", "javascript"],
-  ];
+  const [projects, setProjects] = useState([]);
+  const [tags, setTags] = useState([]);
 
-  const projectsURL = [
-    "https://jayson-wcs.github.io/projet-omnifood/#",
-    "https://jayson-wcs.github.io/projet-travel-agency/#",
-    "https://jayson-wcs.github.io/projet-snake/"
-  ];
+  useEffect(() => {
+    axios
+      .get("http://localhost:3309/projects")
+      .then((response) => {
+        setProjects(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching projects:", error);
+      });
+  }, []);
 
-  const projectsDescription = [
-    "Welcome to Omnifood ! This is a landing page for our premium food delivery service, where we prioritize quality, convenience, and healthy eating.",
-    "Welcome to Travel Agency ! This is a landing page for a travel booking planner agency.",
-    "Welcome to the Snake Game ! This is a JavaScript implementation of the classic Snake game, where you control a snake to eat food and grow longer without hitting the walls or yourself.",
-    "Welcome to Doruido ! This is a personal project based on a passion of mine, Japanimation. It will provide a platform to get cultural news about animes, mangas and Japanese art scene.",
-    "Welcome to the Alibi Hackathon! First participation in the Wild Code School Hackathon, this mini project was carried out in teams of 5 in 24 hours. Landing page for an atypical travel agency... Who knows, you might just find what you're looking for!",
-    "Welcome to Flipper ! Project based on the visual and interactive reproduction of a social network (Twitter). Group work at Wild Code School."
-  ];
-
-  const badgeType = (tag) => {
-    switch (tag) {
-      case "html":
-      case "javascript":
-        return "badge-accent";
-      case "css":
-      case "react":
-        return "badge-primary";
-      case "sass":
-        return "badge-secondary text-black";
-      case "node":
-        return "bg-green-500 text-black";
-      case "mysql":
-        return "badge-base-100";
-      default:
-        return "badge-neutral";
-    }
-  };
+    useEffect(() => {
+    axios
+      .get("http://localhost:3309/tags")
+      .then((response) => {
+        setTags(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching tags:", error);
+      });
+  }, []);
 
   return (
     <div className="flex flex-wrap gap-9 justify-center content-center">
@@ -60,27 +45,34 @@ export default function CardProject() {
           className="card max-w-lg w-full sm:w-full md:w-1/2 lg:w-1/2 shadow-xl"
         >
           <figure>
-            <img className="object-contain" src={projectImages[i]} alt={project} />
+            <img className="object-contain" src={projectImages[i]} alt={project.title} />
           </figure>
           <div className="card-body">
-            <h2 className="card-title mb-5 text-2xl">{project}</h2>
-            <p className="leading-7 w-fit">{projectsDescription[i]}</p>
+            <h2 className="card-title mb-5 text-2xl">{project.title}</h2>
+            <p className="leading-7 w-fit">{project.description}</p>
             <div className="card-actions mt-5">
-              {projectsTags[i].map((tag, i) => (
-                <div data-theme="cmyk"
+              {tags.map((tag, i) => (
+                <div
                   key={i}
-                  className={`badge ${badgeType(tag)} px-5 py-3 font-bold text-lg mb-3`}
+                  className={`badge ${(tag.name)} px-5 py-3 font-bold text-lg mb-3`}
                 >
-                  {tag}
+                  {tag.name}
                 </div>
               ))}
             </div>
-            {projectsURL[i] ? (
-              <a data-theme="cmyk" href={projectsURL[i]} target="_blank" rel="noreferrer" className="btn font-bold text-xl">
+            {project.projectURL ? (
+              <a
+                href={project.projectURL}
+                target="_blank"
+                rel="noreferrer"
+                className="btn font-bold text-xl"
+              >
                 Visit
               </a>
             ) : (
-              <div className="bg-base-100 px-5 py-3 font-bold text-xl text-center h-100">Coming Soon</div>
+              <div className="bg-base-100 px-5 py-3 font-bold text-xl text-center h-100">
+                Coming Soon
+              </div>
             )}
           </div>
         </div>
